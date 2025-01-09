dd64ad01ca49938c9a6ca554d2d5e480a3968ced
sonucom <dk8882612@gmail.com>
2025-01-09 09:07:48 +0530

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,6 @@
+const deepakSONU = document.getElementById('deepakSONU')
+const hlo = document.getElementById('hlo');
+function menubar(){
+ hlo.classList.toggle('hidden')
+ New.classList.toggle('hidden')
+}   
